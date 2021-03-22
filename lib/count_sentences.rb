1a99d6a2dcbc49178a2da4d4ab0c3fae2cffe74c
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-22 15:17:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-  self.split(/\.|\!|\?/).delete_if do |e| 
+  self.split(/\.|\!|\?/).delete_if do |e|
       e.size < 1 
     end.count
   #  binding.pry
